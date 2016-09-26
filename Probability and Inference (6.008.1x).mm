30f9263d367be460013241b551a1245ebfd66514
chienfm <bachien@ejnu.net>
2016-09-26 12:49:11 +0900

    First commit

---
+++ Probability and Inference (6.008.1x).mm
@@ -0,0 +1,86 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Probability and Inference (6.008.1x)" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1474739579576"><hook NAME="MapStyle" zoom="0.826">
+    <properties show_icon_for_attributes="true" show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+<stylenode TEXT="Test" BACKGROUND_COLOR="#9ae2ae" STYLE="bubble">
+<font NAME="Arial" SIZE="10"/>
+<edge STYLE="hide_edge"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="3"/>
+<node TEXT="0. Introduction" LOCALIZED_STYLE_REF="styles.topic" POSITION="right" ID="ID_171035226" CREATED="1474697100434" MODIFIED="1474697951115" HGAP="35" VSHIFT="-46">
+<edge COLOR="#ff0000"/>
+</node>
+<node TEXT="1. Probability and Inference" LOCALIZED_STYLE_REF="styles.topic" POSITION="left" ID="ID_1775591942" CREATED="1474697232430" MODIFIED="1474739516921" HGAP="49" VSHIFT="30">
+<edge COLOR="#0000ff"/>
+<node TEXT="Introduction to Probability" ID="ID_1339476342" CREATED="1474697378339" MODIFIED="1474739433090" HGAP="34" VSHIFT="21"/>
+<node TEXT="Probability Space and Event" ID="ID_1432954341" CREATED="1474697422372" MODIFIED="1474739438623" HGAP="37" VSHIFT="45"/>
+<node TEXT="Random Variable" ID="ID_614542077" CREATED="1474697453721" MODIFIED="1474739504878" HGAP="48" VSHIFT="34"/>
+<node TEXT="Jointy Distributed Random" ID="ID_1214710930" CREATED="1474697496302" MODIFIED="1474739508459" HGAP="33" VSHIFT="39">
+<node TEXT="Relating 2 Random Variables" ID="ID_1174360490" CREATED="1474735160774" MODIFIED="1474739473118" HGAP="40" VSHIFT="21"/>
+<node TEXT="Marginalization" ID="ID_293533676" CREATED="1474735193046" MODIFIED="1474739477025" HGAP="46" VSHIFT="27">
+<node TEXT="2 Variables" ID="ID_792662487" CREATED="1474735292577" MODIFIED="1474735309406" HGAP="-227" VSHIFT="3"/>
+<node TEXT="Many Variables" ID="ID_555659954" CREATED="1474735311595" MODIFIED="1474735330340" HGAP="-261" VSHIFT="19"/>
+</node>
+<node TEXT="Conditioning for Random Variables" ID="ID_1667107207" CREATED="1474735227880" MODIFIED="1474739482629" HGAP="49" VSHIFT="56">
+<node TEXT="Simson&apos;s Paradox" ID="ID_337176983" CREATED="1474738750392" MODIFIED="1474739489466" HGAP="17" VSHIFT="30">
+<node TEXT="\latex lam sao bay gio day&#xa; $x_2 =1$ .Khong the ngu hon" ID="ID_275809155" CREATED="1474742287244" MODIFIED="1474742548370"/>
+</node>
+</node>
+</node>
+<node TEXT="Conditioning and Event" ID="ID_1269641056" CREATED="1474697707410" MODIFIED="1474739516920" HGAP="24" VSHIFT="58"/>
+</node>
+<node TEXT="A" POSITION="right" ID="ID_1360190118" CREATED="1474739531256" MODIFIED="1474739579576" HGAP="51" VSHIFT="26">
+<edge COLOR="#00ff00"/>
+<node TEXT="&#xb9cc;&#xb098; &#xc694;" ID="ID_1413284457" CREATED="1474787352844" MODIFIED="1474787370679"/>
+<node TEXT="new system" ID="ID_348884783" CREATED="1474787379480" MODIFIED="1474787384756"/>
+</node>
+</node>
+</map>
